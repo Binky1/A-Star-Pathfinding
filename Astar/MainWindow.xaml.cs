45d655930ce519b01f52cb4ce99c8795fb7af1a1
ori baron <oribaron100@gmail.com>
2022-02-05 13:01:25 +0200

    Changes

--- Astar/MainWindow.xaml.cs
+++ Astar/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +22,11 @@ namespace Astar
     public partial class MainWindow : Window
     {
         public int gx = 0;
+        public int attempts = 0;
+        public int count = 0;
         public bool obs_on = false;
+        public bool found = false;
+        
 
         public Button[,] Buttons = new Button[4,4];
         public Button startNode, endNode,currentNode;
@@ -29,6 +34,8 @@ namespace Astar
         List <Button> OpenList = new List <Button>();
         List <Button> ClosedList = new List <Button>();
         List <Button> Obstacles = new List <Button>();
+        List <Button> DeadEnd = new List <Button>();
+        List <Button> Path = new List <Button>();
 
         public MainWindow()
         {
@@ -63,34 +70,98 @@ namespace Astar
 
         }
 
+        public void Restart(object sender, RoutedEventArgs e)
+        {
+            OpenList.Clear();
+            Obstacles.Clear();
+            ClosedList.Clear();
+            DeadEnd.Clear();
+            found = false;
+            GenerateButtons();
+        }
         public void Finding()
         {
-
+            attempts++;
+            gx++;
+            count = 0;
             // Getting the cordinated of the current node
             currentNode = ClosedList[ClosedList.Count - 1];
-            int row = FindRow(currentNode); 
+            int row = FindRow(currentNode);
             int column = FindColumn(currentNode);
 
             //TODO improve the for loop
+            Direct(row, column);
+
+            if (OpenList.Contains(endNode))
+            {
+                Console.WriteLine("End node is one of the option");
+            }
+
+
+            if (currentNode != endNode && attempts ==300)
+            {
+                MessageBox.Show("Not Found");
+                Console.WriteLine("Not Found");
+                found = true;
+            }
+            else if (currentNode != endNode)
+            {
+
+
+                // Going to the direction
+                if (OpenList.Count != 0)
+                {
+                    Button bt = FindMin();
+                    //bt.Background = Brushes.Brown;
+                    OpenList.Remove(bt);
+                    ClosedList.Add(bt);
+                }
+
+            }
+
+
+
+            if (Dead(currentNode) == 0)
+            {
+                DeadEnd.Add(currentNode);
+            }
+
+
+            if (currentNode != endNode && !found)
+            {
+                Finding();
+            }
+
+
+            Console.WriteLine(ClosedList.Count);
+
+
+
+        }
+
+
+
+        private void Direct(int row, int column)
+        {
             for (int i = 0; i < 4; i++)
             {
                 try
                 {
 
-                    if (i == 0 && Buttons[row - 1,column] != startNode && !Obstacles.Contains(Buttons[row - 1, column]))
+                    if (i == 0 && Buttons[row - 1, column] != startNode && !Obstacles.Contains(Buttons[row - 1, column]) && !ClosedList.Contains(Buttons[row - 1, column]) && !DeadEnd.Contains(Buttons[row - 1, column]))
                     {
                         Up(row, column);
 
                     }
-                    else if (i == 1 && Buttons[row,column + 1] != startNode && !Obstacles.Contains(Buttons[row, column + 1]))
+                    else if (i == 1 && Buttons[row, column + 1] != startNode && !Obstacles.Contains(Buttons[row, column + 1]) && !ClosedList.Contains(Buttons[row, column + 1]) && !DeadEnd.Contains(Buttons[row, column + 1]))
                     {
                         Right(row, column);
                     }
-                    else if (i == 2 && Buttons[row + 1,column] != startNode && !Obstacles.Contains(Buttons[row + 1, column]))
+                    else if (i == 2 && Buttons[row + 1, column] != startNode && !Obstacles.Contains(Buttons[row + 1, column]) && !ClosedList.Contains(Buttons[row + 1, column]) && !DeadEnd.Contains(Buttons[row + 1, column]))
                     {
                         Bottom(row, column);
                     }
-                    else if (i == 3 && Buttons[row,column - 1] != startNode && !Obstacles.Contains(Buttons[row, column - 1]))
+                    else if (i == 3 && Buttons[row, column - 1] != startNode && !Obstacles.Contains(Buttons[row, column - 1]) && !ClosedList.Contains(Buttons[row, column - 1]) && !DeadEnd.Contains(Buttons[row, column - 1]))
                     {
                         Left1(row, column);
                     }
@@ -105,30 +176,52 @@ namespace Astar
                 }
 
             }
+        }
 
-            if (OpenList.Contains(endNode))
+        private void GetPath()
+        {
+            Path.Add(endNode);
+        }
+
+        private int Dead(Button bt)
+        {
+            int row = FindRow(bt);
+            int column = FindColumn(bt);
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("End node is one of the option");
+                try
+                {
+
+                    if (i == 0 && Buttons[row - 1, column] != startNode && !Obstacles.Contains(Buttons[row - 1, column]) && !ClosedList.Contains(Buttons[row - 1, column]))
+                    {
+                        count++;
+
+                    }
+                    else if (i == 1 && Buttons[row, column + 1] != startNode && !Obstacles.Contains(Buttons[row, column + 1]) && !ClosedList.Contains(Buttons[row, column + 1]))
+                    {
+                        count++;
+                    }
+                    else if (i == 2 && Buttons[row + 1, column] != startNode && !Obstacles.Contains(Buttons[row + 1, column]) && !ClosedList.Contains(Buttons[row + 1, column]))
+                    {
+                        count++;
+                    }
+                    else if (i == 3 && Buttons[row, column - 1] != startNode && !Obstacles.Contains(Buttons[row, column - 1]) && !ClosedList.Contains(Buttons[row, column - 1]))
+                    {
+                        count++;
+                    }
+
+
+
+
+                }
+                catch
+                {
+                    continue;
+                }
+               
+
             }
-
-
-            // Going to the direction
-            Button bt = FindMin();
-            //bt.Background = Brushes.Brown;
-            OpenList.Remove(bt);
-            ClosedList.Add(bt);
-
-
-            if (currentNode != endNode)
-            {
-                Finding();
-            }
-
-
-            Console.WriteLine(ClosedList.Count);
-
-
-            
+            return count;
         }
 
         public Button FindMin()
@@ -173,11 +266,12 @@ namespace Astar
         private void Left1(int row, int column)
         {
 
-                OpenList.Add(Buttons[row,column - 1]);
-                if (Buttons[row, column - 1] != endNode)
-                {
-                    Buttons[row, column - 1].Background = Brushes.LightYellow;
-                }
+            OpenList.Add(Buttons[row,column - 1]);
+            Console.WriteLine("Left");
+            if (Buttons[row, column - 1] != endNode)
+            {
+                Buttons[row, column - 1].Background = Brushes.LightYellow;
+            }
                 
             
         }
@@ -185,11 +279,12 @@ namespace Astar
         private void Bottom(int row, int column)
         {
             
-                OpenList.Add(Buttons[row + 1,column]);
-                if (Buttons[row + 1, column] != endNode)
-                {
-                    Buttons[row + 1, column].Background = Brushes.LightYellow;
-                }
+            OpenList.Add(Buttons[row + 1,column]);
+            Console.WriteLine("Down");
+            if (Buttons[row + 1, column] != endNode)
+            {
+                Buttons[row + 1, column].Background = Brushes.LightYellow;
+            }
 
         }
 
@@ -197,22 +292,24 @@ namespace Astar
         {
 
 
-                OpenList.Add(Buttons[row,column + 1]);
-                if (Buttons[row, column + 1] != endNode)
-                {
-                    Buttons[row, column + 1].Background = Brushes.LightYellow;
-                }
+            OpenList.Add(Buttons[row,column + 1]);
+            Console.WriteLine("Right");
+            if (Buttons[row, column + 1] != endNode)
+            {
+                Buttons[row, column + 1].Background = Brushes.LightYellow;
+            }
 
 
         }
         private void Up(int row, int column)
         {
 
-                OpenList.Add(Buttons[row - 1,column]);
-                if (Buttons[row - 1, column] != endNode)
-                {
-                    Buttons[row - 1, column].Background = Brushes.LightYellow;
-                }
+            OpenList.Add(Buttons[row - 1,column]);
+            Console.WriteLine("Up");
+            if (Buttons[row - 1, column] != endNode)
+            {
+                Buttons[row - 1, column].Background = Brushes.LightYellow;
+            }
 
         }
 
@@ -251,6 +348,7 @@ namespace Astar
         {
             ClosedList.Add(startNode);
             Finding();
+            ClosedList.Add(endNode);
 
             for (int i = 0; i < ClosedList.Count; i++)
             {
